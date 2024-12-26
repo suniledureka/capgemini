f0224fa210a38c55b67347b74555dd6a14960343
suniledureka <sunil.edureka@gmail.com>
2024-12-26 11:43:02 +0530

    day-33

---
+++ java/day-33/codes/07.sb-security-oauth/src/main/java/com/capgemini/boot/security/configs/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.capgemini.boot.security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+	
+ @Bean
+ public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+   httpSecurity.authorizeHttpRequests(request -> request.anyRequest()
+		   												.authenticated());
+   httpSecurity.oauth2Login(Customizer.withDefaults()); 
+   
+   return httpSecurity.build();
+ }
+}
