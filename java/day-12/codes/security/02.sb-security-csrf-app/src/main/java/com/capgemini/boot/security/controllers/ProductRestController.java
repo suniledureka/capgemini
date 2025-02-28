b51fb4199c0bd4088ae2644e6aca7f99674beafc
suniledureka <sunil.edureka@gmail.com>
2025-02-28 10:42:53 +0530

    day-12

---
+++ java/day-12/codes/security/02.sb-security-csrf-app/src/main/java/com/capgemini/boot/security/controllers/ProductRestController.java
@@ -0,0 +1,35 @@
+package com.capgemini.boot.security.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.boot.security.models.Product;
+import com.capgemini.boot.security.repository.ProductRepository;
+
+@RestController
+public class ProductRestController {
+
+	private List<Product> productsList = ProductRepository.getProducts();
+	
+	@GetMapping("/products")
+	public List<Product> getAllProducts(){  
+		return productsList; 
+	}
+	
+	@GetMapping("/products/{pid}")
+	public Product getProductByProductId(@PathVariable("pid") Integer pid) {
+	   Product product = productsList.stream().filter(prod -> prod.productId().equals(pid)).findFirst().orElse(null);
+	   return product; 
+	}	
+	
+	@PostMapping("/products/new")
+	public void addNewProduct(@RequestBody Product product) {
+	   productsList.add(product);	   
+	}	
+}
