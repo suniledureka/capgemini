0fa025e9682404573ec3f80bfa6c05ad03b4a4bc
suniledureka <sunil.edureka@gmail.com>
2025-03-01 15:11:21 +0530

    removed

--- java/day-13/codes/part-5/accounts/src/main/java/com/capgemini/accounts/service/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.accounts.service;
-
-import com.capgemini.accounts.dto.CustomerDetailsDto;
-
-public interface ICustomerService {
-	
-	CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
-
-}
